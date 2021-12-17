ed65f1b47571ac47f94453bbff6d8e28100c6de2
Francisco Fernandez Diez <francisco.fernandezdiez@viseo.com>
2021-12-17 17:13:18 +0800

    add subnet

--- main.tf
+++ main.tf
@@ -28,3 +28,10 @@ resource "azurerm_virtual_network" "myTerraformNetwork" {
       environment       = "Terraform Demo"
   }
 }
+
+resource "azurerm_subnet" "myTerraformSubnet" {
+  name                  = "mySubnet"
+  resource_group_name   = azurerm_resource_group.myResourceGroup.name
+  virtual_network_name  = azurerm_virtual_network.myTerraformNetwork.name
+  address_prefixes      = ["10.0.1.0/24"]
+}
